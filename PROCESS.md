3450126924d3498e110575b8242b0a53809a858c
Sithu <sithu.sithuthedev@gmail.com>
2024-01-04 19:36:33 +0630

    install tailwind css

---
+++ PROCESS.md
@@ -0,0 +1,3 @@
+## Navbar
+
+- To build navbar I want to try tailwind CSS, so now I will install tailwind CSS in my vite project.
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello Devs!✌️</h1>
+      <Navbar />
+      {/* <h1>Hello Devs!✌️</h1> */}
+      <h1>Hi, I'm Sithu Aung Khant</h1>
+      <p>A Front End Developer</p>
     </>
   );
 }
