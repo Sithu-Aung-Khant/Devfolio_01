e68d51f6453101ace2a27cd93c12061bbd40281c
Sithu <sithu.sithuthedev@gmail.com>
2024-01-11 23:13:37 +0630

    Implement shortcut key functionality for toggling between dark and light mode

--- src/components/head/Toggle.jsx
+++ src/components/head/Toggle.jsx
@@ -1,8 +1,22 @@
 // Dark/Light Theme Switcher Toggle
 
 import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+import { useEffect } from 'react';
 
 const Toggle = ({ onToggle, isDark }) => {
+  const handleKeyPress = (event) => {
+    if (event.key === 'm' && event.ctrlKey) {
+      onToggle();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [onToggle]);
+
   return (
     <>
       <button onClick={onToggle} className="h-6 w-6 text-black-500 -ms-2">
