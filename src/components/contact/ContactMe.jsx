8d708187257c2f92470937236101aa68509038ee
Sithu <sithu.sithuthedev@gmail.com>
2024-01-20 20:07:27 +0630

    Add EmailJS for contact form functionality

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_nejaw98",
+        "template_9j8w10t",
+        form.current,
+        "W6q8zyo65G01qqIUG",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+  };
+
   return (
     <section id="contact_me">
-      <div className="container mx-auto text-center h-96">
-        <div className="text-3xl font-semibold">Contact Me</div>
+      <div className="container mx-auto h-96">
+        <div className="text-3xl font-semibold text-center">Contact Me</div>
+        <div className="md:flex">
+          <div className="md:w-1/2"></div>
+          <div className="md:w-1/2">
+            <form ref={form} onSubmit={sendEmail}>
+              <label className="hidden">Name</label>
+              <input
+                type="text"
+                placeholder="Name"
+                name="user_name"
+                className="block w-full border p-2 rounded-lg my-2"
+              />
+              <label className="hidden">Email</label>
+              <input
+                type="email"
+                placeholder="Email"
+                name="user_email"
+                className="block w-full border p-2 rounded-lg my-2"
+              />
+              <label className="hidden">Message</label>
+              <textarea
+                placeholder="Your Message"
+                name="message"
+                className="bg-slate-200 p-4 rounded-lg w-full block h-full"
+              />
+
+              <input
+                type="submit"
+                value="Send"
+                className="bg-slate-300 w-full mt-4 px-4 py-2 rounded-lg"
+              />
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   );
