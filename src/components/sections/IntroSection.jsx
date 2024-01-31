33b97b3efaf3fe35bae1435e475ac36784c63bd9
Sithu <sithu.sithuthedev@gmail.com>
2024-01-31 20:22:01 +0630

    refactor: Convert 'Sithu-thedev' text to clickable link

--- src/components/sections/IntroSection.jsx
+++ src/components/sections/IntroSection.jsx
@@ -4,7 +4,7 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 const IntroSection = () => {
   return (
     <section id="intro">
-      <div className="container-2xl mx-auto p-4 md:h-dvh lg:px-24 xl:px-40 2xl:px-72">
+      <div className="container-2xl mx-auto p-4 md:h-dvh lg:px-24 xl:px-40 2xl:px-72 pt-24">
         <div className="hidden md:flex justify-end">Developer Portfolio</div>
         <div className="md:hidden absolute right-0 top-72 font-medium">
           Developer Portfolio
@@ -29,7 +29,13 @@ const IntroSection = () => {
             </div>
           </div>
         </div>
-        <div className="hidden md:block absolute pt-10">@Sithu-thedev</div>
+        <a
+          href="https://github.com/Sithu-Aung-Khant"
+          target="_blank"
+          className="hidden md:block absolute pt-10"
+        >
+          @Sithu-thedev
+        </a>
 
         <div className="hidden md:block absolute right-0">
           <div className="flex items-center py-8 md:py-0">
