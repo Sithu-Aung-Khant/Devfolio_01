6e2c74bcaf5f583f742045e213ea6e65fdcc783d
Sithu <sithu.sithuthedev@gmail.com>
2024-02-04 23:33:45 +0630

    Refactor: transform portfolio projects design into reusable components.

--- src/components/ProjectDesign.jsx
+++ src/components/ProjectDesign.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProjectDesignRight = ({
+const ProjectCard = ({
   id,
   title,
   description,
@@ -10,30 +10,31 @@ const ProjectDesignRight = ({
   imagePath,
 }) => {
   return (
-    <section id={`project${id}`}>
-      <div className="flex justify-start pt-12 md:pt-0 md:-mb-10">
-        <div className="w-20 h-3 md:w-40 md:h-5 bg-brown03 dark:bg-dim_white md:ml-10"></div>
-      </div>
-      <div className="container mx-auto px-0 md:pt-4 md:h-dvh flex flex-col justify-center">
+    <section id={id}>
+      <div className="container mx-auto px-0 md:pt-4 flex flex-col justify-center">
+        <div className="flex items-center py-6">
+          <h1 className="text-4xl md:text-7xl pl-4 lg:pl-24 pr-8">Projects</h1>
+          <div className="h-0.5 bg-black dark:bg-dim_white w-full"></div>
+        </div>
         <div className="md:flex items-center mx-auto max-w-screen-lg">
           <div className="md:w-1/2 px-2 md:pr-4 md:pl-16">
             <img className="" src={imagePath} alt="" />
           </div>
           <div className="md:w-1/2 px-6">
-            <h1 className="text-4xl font-medium">{title}</h1>
+            <h1 className="text-3xl md:text-4xl font-medium">{title}</h1>
             <div className="w-14 h-1 bg-brown03 dark:bg-white my-4"></div>
             <p>{description}</p>
-            <ul className="list-disc pl-4 pt-1 pb-6">
+            <ul className="list-disc pl-4 py-2">
               {builtWith.map((item, index) => (
                 <li key={index}>{item}</li>
               ))}
             </ul>
-            <button className="bg-brown03 text-dim_white border border-brown03  dark:bg-white dark:text-black dark:hover:bg-brown04  px-4 py-2 font-medium">
+            <button className="bg-brown03 text-dim_white border border-brown03  dark:bg-white dark:text-black dark:hover:bg-brown04  px-4 py-2 mt-4 font-medium">
               <a href={caseStudyLink} target="_blank" rel="noopener noreferrer">
                 Case Study
               </a>
             </button>
-            <button className="px-4 py-2 ml-3 border border-brown03 dark:border-white dark:text-dim_white hover:underline">
+            <button className="px-4 py-2 ml-3 border border-brown03 dark:border-white dark:text-dim_white hover:underline ">
               <a href={liveSiteLink} target="_blank" rel="noopener noreferrer">
                 Live Site
               </a>
@@ -45,4 +46,4 @@ const ProjectDesignRight = ({
   );
 };
 
-export default ProjectDesignRight;
+export default ProjectCard;
--- src/pages/home/Projects.jsx
+++ src/pages/home/Projects.jsx
@@ -1,13 +1,23 @@
 import InsureLandingPage from "../projects/InsureLandingPage";
-import ImageSearchApp from "../projects/ImageSearchApp";
-import AgeCalculator from "../projects/AgeCalculator";
+import ProjectCard from "@/components/ProjectDesign";
 import ProjectDesignLeft from "@/components/ProjectDesignLeft";
 import ProjectDesignRight from "@/components/ProjectDesignRight";
 
 const Projects = () => {
   return (
     <section id="projects">
-      <InsureLandingPage />
+      <ProjectCard
+        id="social-media-dashboard"
+        title="Social Media Dashboard"
+        description="Social Media Dashboard with a seamless Dark to Light Mode switch, designed to enhance user interaction and customization."
+        builtWith={[
+          "Built With: SASS, JavaScript, Gulp",
+          "Dynamic Dark and Light Mode",
+        ]}
+        caseStudyLink="https://github.com/Sithu-Aung-Khant/social-media-dashboard"
+        liveSiteLink="https://social-media-dashboard-sithu-thedev.netlify.app/"
+        imagePath="/projects/social-media-dashboard/social-media-dashboard.png"
+      />
       {/* Image Search App */}
       <ProjectDesignLeft
         id="image-search-app"
@@ -21,8 +31,19 @@ const Projects = () => {
         liveSiteLink="https://image-search-app-by-sithu-thedev.netlify.app/"
         imagePath="/projects/image-search/894.png"
       />
-      <ImageSearchApp />
-      <AgeCalculator />
+      {/* <InsureLandingPage /> */}
+      <ProjectDesignRight
+        id="insure-landing-page"
+        title="Insure: Landing Page"
+        description="Fully responsive insurance website, experience seamless display across diverse screen sizes, ensuring optimal display across various screen sizes."
+        builtWith={[
+          "Built With: React-Bootstrap, JavaScript",
+          "Fully Responsive",
+        ]}
+        caseStudyLink="https://github.com/Sithu-Aung-Khant/insure-landing-page"
+        liveSiteLink="https://insure-insurance-company.netlify.app/"
+        imagePath="./projects/insure-landing-page/894.png"
+      />
       {/* <div className="flex items-center">
           <div className="h-0.5 bg-black w-full"></div>
           <h1 className="text-4xl md:text-7xl pr"-8">Portfolio</h1>
